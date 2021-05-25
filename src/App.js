2e8f873944792b9d3ac6092833315675070295f8
jokale <kalemajoanna@gmail.com>
2021-05-25 16:46:08 +0100

    add image for background

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Countdown from './components/Countdown';
 import Navbar from "./components/Navbar";
+import background from "./images/background.jpg";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
      <center> <h1>App</h1></center>
      <Countdown/>
+     <img src={background} className="backgroundimg" alt="clothing hanging" />
 
     </div>
   );
