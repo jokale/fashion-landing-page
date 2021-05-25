10417d1480e658e41fe6c53bb3ba6f32586f4349
jokale <kalemajoanna@gmail.com>
2021-05-25 16:39:28 +0100

    render components in app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 // import './App.css';
+import React from 'react';
+import Countdown from './components/Countdown';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
      <center> <h1>App</h1></center>
+     <Countdown/>
+
     </div>
   );
 }
